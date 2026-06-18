2087ce282abf180fccceed4f832314737a6460e2
yura1685 <yura888826@gmail.com>
2026-06-18 05:39:26 +0000

    [AC] abc004 abc004_2

---
+++ atcoder.jp/yura1685/abc004/abc004_2/56655276.py
@@ -0,0 +1,9 @@
+c1 = list(map(str,input().split()))
+c2 = list(map(str,input().split()))
+c3 = list(map(str,input().split()))
+c4 = list(map(str,input().split()))
+
+print(c4[3],c4[2],c4[1],c4[0])
+print(c3[3],c3[2],c3[1],c3[0])
+print(c2[3],c2[2],c2[1],c2[0])
+print(c1[3],c1[2],c1[1],c1[0])
