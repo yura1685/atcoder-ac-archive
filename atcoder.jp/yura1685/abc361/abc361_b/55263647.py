f3622ec3f71c2c096953b6beec044b8be76ebc4a
yura1685 <yura888826@gmail.com>
2026-06-18 05:14:28 +0000

    [AC] abc361 abc361_b

---
+++ atcoder.jp/yura1685/abc361/abc361_b/55263647.py
@@ -0,0 +1,8 @@
+a,b,c,d,e,f=map(int,input().split())
+g,h,i,j,k,l=map(int,input().split())
+if a<j and g<d:
+    if b<k and h<e:
+        if c<l and i<f:
+            print('Yes')
+            exit()
+print('No')
