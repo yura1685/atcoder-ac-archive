3ba80b1fa7f212eef50797c5d10baefb60cbbb90
yura1685 <yura888826@gmail.com>
2026-06-18 05:45:55 +0000

    [AC] cf17-final cf17_final_a

---
+++ atcoder.jp/yura1685/cf17-final/cf17_final_a/58346720.py
@@ -0,0 +1,7 @@
+S = input()
+s = ['AKIHABARA','KIHABARA','AKIHBARA','AKIHABRA','AKIHABAR','KIHBARA','KIHABRA','KIHABAR','AKIHBRA','AKIHBAR','AKIHABR','KIHBRA','KIHBAR','KIHABR','AKIHBR','KIHBR']
+for i in s:
+    if i == S:
+        print('YES')
+        exit()
+print('NO')
