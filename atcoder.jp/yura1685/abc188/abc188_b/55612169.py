4a2caf04a8eb92d541a973033a48e7d78fe13395
yura1685 <yura888826@gmail.com>
2026-06-18 05:22:44 +0000

    [AC] abc188 abc188_b

---
+++ atcoder.jp/yura1685/abc188/abc188_b/55612169.py
@@ -0,0 +1,10 @@
+N = int(input())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+a = 0
+for i in range(N):
+    a += A[i]*B[i]
+if a == 0:
+    print('Yes')
+else:
+    print('No')
