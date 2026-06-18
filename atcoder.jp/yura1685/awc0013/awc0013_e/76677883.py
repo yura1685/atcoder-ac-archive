779bb745d88bc9ae45ca17da0a0d08f4eb1e4e3f
yura1685 <yura888826@gmail.com>
2026-06-18 08:23:07 +0000

    [AC] awc0013 awc0013_e

---
+++ atcoder.jp/yura1685/awc0013/awc0013_e/76677883.py
@@ -0,0 +1,16 @@
+from atcoder.maxflow import MFGraph
+
+N, M = map(int, input().split())
+g = MFGraph(N + M + 2)
+for i in range(1, N+1):
+    g.add_edge(0, i, 1)
+for i in range(N+1, N+M+1):
+    g.add_edge(i, N+M+1, 1)
+
+for i in range(1, N+1):
+    K, *C = map(int, input().split())
+    for j in C:
+        g.add_edge(i, j+N, 1)
+    
+ans = g.flow(0, N+M+1)
+print(ans)
