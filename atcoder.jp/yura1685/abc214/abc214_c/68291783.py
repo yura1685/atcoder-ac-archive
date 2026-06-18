56352d6993194db44a6453b77122c44466e95231
yura1685 <yura888826@gmail.com>
2026-06-18 06:43:54 +0000

    [AC] abc214 abc214_c

---
+++ atcoder.jp/yura1685/abc214/abc214_c/68291783.py
@@ -0,0 +1,21 @@
+import heapq
+
+N = int(input())
+S = list(map(int,input().split()))
+T = list(map(int,input().split()))
+
+hq = [(T[i],i) for i in range(N)]
+heapq.heapify(hq)
+
+c = [0]*N
+ok = 0
+
+while ok < N:
+    t, m = heapq.heappop(hq)
+    if c[m] == 0:
+        c[m] = t
+        ok += 1
+    heapq.heappush(hq,(t + S[m],(m+1) % N))
+
+for i in c:
+    print(i)
