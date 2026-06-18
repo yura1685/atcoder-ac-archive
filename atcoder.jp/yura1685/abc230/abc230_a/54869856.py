6a5b6f74cb1f6e558af449819fbfe90745608c5a
yura1685 <yura888826@gmail.com>
2026-06-18 05:10:59 +0000

    [AC] abc230 abc230_a

---
+++ atcoder.jp/yura1685/abc230/abc230_a/54869856.py
@@ -0,0 +1,7 @@
+N = int(input())
+if 1 <= N <= 9:
+  print("AGC"+ str(0) + str(0)+ str(N))
+elif 10 <= N <= 41:
+  print("AGC"+ str(0) + str(N))
+elif 42 <= N <= 54:
+  print("AGC"+ str(0) + str(N+1))
