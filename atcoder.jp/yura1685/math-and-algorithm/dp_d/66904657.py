09ec3409693989bc2f154fc4b306a7d101eea810
yura1685 <yura888826@gmail.com>
2026-06-18 06:29:54 +0000

    [AC] math-and-algorithm dp_d

---
+++ atcoder.jp/yura1685/math-and-algorithm/dp_d/66904657.py
@@ -0,0 +1,13 @@
+N, W = map(int,input().split())
+
+dp = [[0]*(W+1) for _ in range(N+1)]
+
+for i in range(N):
+    w, v = map(int,input().split())
+    for j in range(W+1):
+        if j < w:
+            dp[i+1][j] = dp[i][j]
+        else:
+            dp[i+1][j] = max(dp[i][j], dp[i][j-w]+v)
+
+print(dp[-1][-1])
