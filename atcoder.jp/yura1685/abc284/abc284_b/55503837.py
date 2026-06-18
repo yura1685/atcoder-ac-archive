5f3e4f5e65d1da065159ae9cdb35e39e25cfabfe
yura1685 <yura888826@gmail.com>
2026-06-18 05:22:13 +0000

    [AC] abc284 abc284_b

---
+++ atcoder.jp/yura1685/abc284/abc284_b/55503837.py
@@ -0,0 +1,9 @@
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    A = list(map(int,input().split()))
+    ans = 0
+    for i in range(N):
+        if A[i] % 2 == 1:
+            ans += 1
+    print(ans)
