425b815657a7c1f0c7048235168b6f641d1e690a
yura1685 <yura888826@gmail.com>
2026-06-18 05:23:06 +0000

    [AC] abc184 abc184_b

---
+++ atcoder.jp/yura1685/abc184/abc184_b/55614302.py
@@ -0,0 +1,10 @@
+N, X = map(int,input().split())
+S = input()
+score = X
+for i in range(N):
+    if S[i] == 'o':
+        score += 1
+    else:
+        if score != 0:
+            score -= 1
+print(score)
