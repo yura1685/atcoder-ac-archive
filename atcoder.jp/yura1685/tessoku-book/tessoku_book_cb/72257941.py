ffd49ac8e7c558460efe93def31729b6aef20156
yura1685 <yura888826@gmail.com>
2026-06-18 07:15:06 +0000

    [AC] tessoku-book tessoku_book_cb

---
+++ atcoder.jp/yura1685/tessoku-book/tessoku_book_cb/72257941.py
@@ -0,0 +1,8 @@
+N = int(input())
+A = list(map(int,input().split()))
+for i in range(N-2):
+  for j in range(i+1,N-1):
+    for k in range(j+1,N):
+      if A[i] + A[j] + A[k] == 1000:
+        exit(print('Yes'))
+print('No')
