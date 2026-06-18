1a35d8406cbe27db3fee866dda9afc40cfe7ed96
yura1685 <yura888826@gmail.com>
2026-06-18 05:46:41 +0000

    [AC] abc162 abc162_c

---
+++ atcoder.jp/yura1685/abc162/abc162_c/59734619.py
@@ -0,0 +1,11 @@
+import math
+
+K = int(input())
+ans = 0
+
+for a in range(1,K+1):
+    for b in range(1,K+1):
+        for c in range(1,K+1):
+            ans += math.gcd(a,math.gcd(b,c))
+
+print(ans)
