54c7e3cf408b2dbc794c44155aff0300be6691ed
yura1685 <yura888826@gmail.com>
2026-06-18 07:32:27 +0000

    [AC] abc405 abc405_e

---
+++ atcoder.jp/yura1685/abc405/abc405_e/73417523.py
@@ -0,0 +1,31 @@
+class Combination:
+    def __init__(self, size: int, mod: int):
+        self.size = size
+        self.mod = mod
+        self.fact = [1] * (size + 1)
+        for i in range(1, size + 1):
+            self.fact[i] = (self.fact[i-1] * i) % mod
+        self.factN = self.fact[size]
+        self.revFact = [1] * (size + 1)
+        self.revFact[size] = pow(self.factN, -1, mod)
+        for i in reversed(range(1, size)):
+            self.revFact[i] = (self.revFact[i+1] * (i + 1)) % mod
+    
+    def comb(self, r: int) -> int:
+        if r < 0 or self.size < r: return 0
+        return (self.factN * self.revFact[self.size - r] * self.revFact[r]) % self.mod
+    
+    def combNR(self, N: int, r: int) -> int:
+        if N > self.size: return 0
+        if r < 0 or N < r: return 0
+        return (self.fact[N] * self.revFact[N - r] * self.revFact[r]) % self.mod
+
+A, B, C, D = map(int,input().split())
+mod = 998244353
+
+x = Combination(A+B+C+D, mod)
+ans = 0
+for i in range(C+1):
+    ans = (ans + x.combNR(A+B+i, B)*x.combNR(D+C-1-i, D-1)) % mod
+
+print(ans)
