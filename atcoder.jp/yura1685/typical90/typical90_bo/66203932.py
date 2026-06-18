d1664c8a9ad3969a6561a9f2263b1034224ad4ce
yura1685 <yura888826@gmail.com>
2026-06-18 06:15:01 +0000

    [AC] typical90 typical90_bo

---
+++ atcoder.jp/yura1685/typical90/typical90_bo/66203932.py
@@ -0,0 +1,25 @@
+def f(n):
+    if n == 0:
+        return 0
+    n = str(n)
+    ju = 0
+    for i in range(len(n)):
+        ju += int(n[i])*8**(len(n)-i-1)
+    nine = []
+    cnt = 1
+    while ju != 0:
+        c = ju % (9**cnt)//(9**(cnt-1))
+        ju -= c*9**(cnt-1)
+        if c == 8:
+            c = 5
+        nine.append(str(c))
+        cnt += 1
+
+    nine = list(reversed(nine))
+    return int(''.join(nine))
+
+N, K = map(int,input().split())
+for _ in range(K):
+    N = f(N)
+
+print(N)
