9e0a192310385f1e483f8f5593bd633c9febfc33
yura1685 <yura888826@gmail.com>
2026-06-18 06:19:06 +0000

    [AC] abc328 abc328_d

---
+++ atcoder.jp/yura1685/abc328/abc328_d/66401325.py
@@ -0,0 +1,16 @@
+S = input()
+stack = []
+l = 0
+for s in S:
+    if s != 'C':
+        stack.append(s)
+        l += 1
+    else:
+        if l >= 2 and stack[-2:] == ['A','B']:
+            stack.pop()
+            stack.pop()
+            l -= 2
+        else:
+            stack.append(s)
+            l += 1
+print(''.join(stack))
