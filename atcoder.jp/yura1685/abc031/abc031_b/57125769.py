05e3695f03ef0a2ced78477e4eb5d6f65293a6a2
yura1685 <yura888826@gmail.com>
2026-06-18 05:41:03 +0000

    [AC] abc031 abc031_b

---
+++ atcoder.jp/yura1685/abc031/abc031_b/57125769.py
@@ -0,0 +1,10 @@
+L, H = map(int,input().split())
+N = int(input())
+for i in range(N):
+    A = int(input())
+    if A > H:
+        print(-1)
+    elif L <= A <= H:
+        print(0)
+    else:
+        print(L-A)
