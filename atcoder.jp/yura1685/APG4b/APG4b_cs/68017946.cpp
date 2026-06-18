83706774fbc41f4907983a27c5c80918e57f1eb0
yura1685 <yura888826@gmail.com>
2026-06-18 06:42:32 +0000

    [AC] APG4b APG4b_cs

---
+++ atcoder.jp/yura1685/APG4b/APG4b_cs/68017946.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int seconds = 365 * 24 * 60 * 60;
+
+  cout << seconds << endl;
+  cout << 2*seconds << endl;
+  cout << 5*seconds << endl;
+  cout << 10*seconds << endl;
+
+}
